a893d3a0adf15c7a41827a85d61b24e7bc0db974
edilson gomes de lima <edskateicevida@gmail.com>
2023-10-07 07:22:32 -0300

    Variáveis  com nome

---
+++ Vpp.js
@@ -0,0 +1,4 @@
+var ano = 2023;
+document.write("Adriano nasceu: " + (ano - 10) + "");
+document.write("<br></br>");
+document.write(" Paulo nasceu em " + (ano - 32) + "");
---
+++ app.js
@@ -0,0 +1,2 @@
+let nome = [true,45,"edy", 10];
+console.log(nome[3]);
